05eb0430301aeb98bc12e3942facc8f51613c682
SUHONG\topic <topic828@Naver.com>
2023-02-01 18:47:36 +0900

    [fix] vercel 주소로 변경

--- src/pages/socialLogin/KakaoData.js
+++ src/pages/socialLogin/KakaoData.js
@@ -2,4 +2,4 @@
 export const REST_API_KEY = "77710050d2af99e71fcbae4838a1302a";
 
 // export const redirect_uri = "http://localhost:3000/kakaologin";
-export const REDIRECT_URI = "http://localhost:3000/KaKaoLogin";
+export const REDIRECT_URI = "https://front-clone-insane.vercel.app/KaKaoLogin";
